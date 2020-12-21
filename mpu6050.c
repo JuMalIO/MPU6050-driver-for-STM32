d2179a80661a152866541555135916853b335ab5
JuMalIO <44319139+JuMalIO@users.noreply.github.com>
2020-12-21 14:58:12 +0200

    Update mpu6050.c

--- mpu6050.c
+++ mpu6050.c
@@ -8,12 +8,12 @@
 extern I2C_HandleTypeDef hi2c1;
 
 void ReadByte(uint8_t address, uint8_t subAddress, uint8_t* dest)
-{  
+{	
 	HAL_I2C_Mem_Read(&hi2c1, address, subAddress, 1, dest, 1, 100);
 }
 
 void ReadBytes(uint8_t address, uint8_t subAddress, uint8_t count, uint8_t* dest)
-{  
+{	
 	HAL_I2C_Mem_Read(&hi2c1, address, subAddress, 1, dest, count, 100);
 }
 
@@ -56,9 +56,9 @@ void WriteBit(uint8_t devAddr, uint8_t regAddr, uint8_t bitNum, uint8_t data)
 
 void WriteBits(uint8_t devAddr, uint8_t regAddr, uint8_t bitStart, uint8_t length, uint8_t data)
 {
-	//      010 value to write
+	//			010 value to write
 	// 76543210 bit numbers
-	//    xxx   args: bitStart=4, length=3
+	//		xxx	 args: bitStart=4, length=3
 	// 00011100 mask byte
 	// 10101111 original value (sample)
 	// 10100011 original & ~mask
@@ -77,9 +77,9 @@ void ReadBits(uint8_t devAddr, uint8_t regAddr, uint8_t bitStart, uint8_t length
 {
 	// 01101001 read byte
 	// 76543210 bit numbers
-	//    xxx   args: bitStart=4, length=3
-	//    010   masked
-	//   -> 010 shifted
+	//		xxx	 args: bitStart=4, length=3
+	//		010	 masked
+	//	 -> 010 shifted
 	uint8_t b;
 	ReadByte(devAddr, regAddr, &b);
 	uint8_t mask = ((1 << length) - 1) << (bitStart - length + 1);
@@ -312,9 +312,9 @@ _Bool MPU6050_Init(void)
 	}
 
 	MPU6050_SetClockSource(MPU6050_CLOCK_PLL_XGYRO);
-  MPU6050_SetFullScaleGyroRange(MPU6050_GYRO_FS_250);
-  MPU6050_SetFullScaleAccelRange(MPU6050_ACCEL_FS_2);
-  MPU6050_SetSleepEnabled(0);
+	MPU6050_SetFullScaleGyroRange(MPU6050_GYRO_FS_250);
+	MPU6050_SetFullScaleAccelRange(MPU6050_ACCEL_FS_2);
+	MPU6050_SetSleepEnabled(0);
 
 	return 0;
 }
@@ -326,14 +326,14 @@ _Bool MPU6050_DmpInit(void)
 		return 1;
 	}
 	
-  // Reset procedure per instructions in the "MPU-6000/MPU-6050 Register Map and Descriptions" page 41
+	// Reset procedure per instructions in the "MPU-6000/MPU-6050 Register Map and Descriptions" page 41
 	WriteBit(MPU6050_ADDRESS, 0x6B, 7, 0x01); //PWR_MGMT_1: reset with 100ms delay
 	
 	HAL_Delay(100);
 
 	WriteBits(MPU6050_ADDRESS, 0x6A, 2, 3, 0x07); // full SIGNAL_PATH_RESET: with another 100ms delay
 	
-	HAL_Delay(100);         
+	HAL_Delay(100);				 
 	
 	WriteByte(MPU6050_ADDRESS, 0x6B, 0x01); // 1000 0001 PWR_MGMT_1:Clock Source Select PLL_X_gyro
 
@@ -341,7 +341,7 @@ _Bool MPU6050_DmpInit(void)
 
 	WriteByte(MPU6050_ADDRESS, 0x23, 0x00); // 0000 0000 MPU FIFO_EN: (all off) Using DMP's FIFO instead
 	
-	WriteByte(MPU6050_ADDRESS, 0x1C, 0x00); // 0000 0000 ACCEL_CONFIG: 0 =  Accel Full Scale Select: 2g
+	WriteByte(MPU6050_ADDRESS, 0x1C, 0x00); // 0000 0000 ACCEL_CONFIG: 0 =	Accel Full Scale Select: 2g
 	
 	WriteByte(MPU6050_ADDRESS, 0x37, 0x80); // 1001 0000 INT_PIN_CFG: ACTL The logic level for int pin is active low. and interrupt status bits are cleared on any read
 	
@@ -349,7 +349,7 @@ _Bool MPU6050_DmpInit(void)
 
 	WriteByte(MPU6050_ADDRESS, 0x19, 0x04); // 0000 0100 SMPLRT_DIV: Divides the internal sample rate 400Hz ( Sample Rate = Gyroscope Output Rate / (1 + SMPLRT_DIV))
 
-	WriteByte(MPU6050_ADDRESS, 0x1A, 0x01); // 0000 0001 CONFIG: Digital Low Pass Filter (DLPF) Configuration 188HZ  //Im betting this will be the beat
+	WriteByte(MPU6050_ADDRESS, 0x1A, 0x01); // 0000 0001 CONFIG: Digital Low Pass Filter (DLPF) Configuration 188HZ	//Im betting this will be the beat
 	
 	MPU6050_WriteMemoryBlock(DmpMemory, MPU6050_DMP_CODE_SIZE, 0, 0);
 	
@@ -361,9 +361,9 @@ _Bool MPU6050_DmpInit(void)
 	
 	WriteByte(MPU6050_ADDRESS, 0x38, 0x02); // 0000 0010 INT_ENABLE: RAW_DMP_INT_EN on
 	
-	WriteBit(MPU6050_ADDRESS, 0x6A, 2, 1);      // Reset FIFO one last time just for kicks. (MPUi2cWrite reads 0x6A first and only alters 1 bit and then saves the byte)
+	WriteBit(MPU6050_ADDRESS, 0x6A, 2, 1);			// Reset FIFO one last time just for kicks. (MPUi2cWrite reads 0x6A first and only alters 1 bit and then saves the byte)
 
-  MPU6050_SetDMPEnabled(0); // disable DMP for compatibility with the MPU6050 library
+	MPU6050_SetDMPEnabled(0); // disable DMP for compatibility with the MPU6050 library
 	
 	return 0;
 }
@@ -476,14 +476,14 @@ float Map(float val, float iMin, float iMax, float oMin, float oMax)
 
 void MPU6050_CalibrateGyro(uint8_t loops)
 {
-  float kP = 0.3;
-  float kI = 90;
-  float x;
-  x = (100 - Map(loops, 1, 5, 20, 0)) * .01f;
-  kP *= x;
-  kI *= x;
-  
-  PID(0x43, kP, kI, loops);
+	float kP = 0.3;
+	float kI = 90;
+	float x;
+	x = (100 - Map(loops, 1, 5, 20, 0)) * .01f;
+	kP *= x;
+	kI *= x;
+
+	PID(0x43, kP, kI, loops);
 }
 
 void MPU6050_CalibrateAccel(uint8_t loops)
@@ -509,7 +509,7 @@ void MPU6050_DmpGetQuaternionInt16(int16_t* data, const uint8_t* packet)
 void MPU6050_DmpGetQuaternion(MPU6050_Quaternion* q, const uint8_t* packet)
 {
 	int16_t qI[4];
-  MPU6050_DmpGetQuaternionInt16(qI, packet);
+	MPU6050_DmpGetQuaternionInt16(qI, packet);
 	q->w = (float)qI[0] / 16384.0f;
 	q->x = (float)qI[1] / 16384.0f;
 	q->y = (float)qI[2] / 16384.0f;
@@ -532,9 +532,9 @@ void MPU6050_DmpGetGravity(MPU6050_VectorFloat* v, MPU6050_Quaternion* q)
 
 void MPU6050_DmpGetEuler(float* data, MPU6050_Quaternion* q)
 {
-	data[0] = atan2(2 * q->x * q->y - 2 * q->w * q->z, 2 * q->w * q->w + 2 * q->x * q->x - 1);   // psi
-	data[1] = -asin(2 * q->x * q->z + 2 * q->w * q->y);                                          // theta
-	data[2] = atan2(2 * q->y * q->z - 2 * q->w * q->x, 2 * q->w * q->w + 2 * q->z * q->z - 1);   // phi
+	data[0] = atan2(2 * q->x * q->y - 2 * q->w * q->z, 2 * q->w * q->w + 2 * q->x * q->x - 1);	 // psi
+	data[1] = -asin(2 * q->x * q->z + 2 * q->w * q->y);																					// theta
+	data[2] = atan2(2 * q->y * q->z - 2 * q->w * q->x, 2 * q->w * q->w + 2 * q->z * q->z - 1);	 // phi
 }
 
 void MPU6050_DmpGetYawPitchRoll(float* data, MPU6050_Quaternion* q, MPU6050_VectorFloat* gravity)
@@ -577,9 +577,9 @@ void GetConjugate(MPU6050_Quaternion* q)
 void GetProduct(MPU6050_Quaternion* res, MPU6050_Quaternion* qo, MPU6050_Quaternion* q)
 {
 	res->w = qo->w * q->w - qo->x * q->x - qo->y * q->y - qo->z * q->z;
-  res->x = qo->w * q->x + qo->x * q->w + qo->y * q->z - qo->z * q->y;
-  res->y = qo->w * q->y - qo->x * q->z + qo->y * q->w + qo->z * q->x;
-  res->z = qo->w * q->z + qo->x * q->y - qo->y * q->x + qo->z * q->w;
+	res->x = qo->w * q->x + qo->x * q->w + qo->y * q->z - qo->z * q->y;
+	res->y = qo->w * q->y - qo->x * q->z + qo->y * q->w + qo->z * q->x;
+	res->z = qo->w * q->z + qo->x * q->y - qo->y * q->x + qo->z * q->w;
 }
 
 void Rotate(MPU6050_VectorInt16* v, MPU6050_Quaternion* q)
