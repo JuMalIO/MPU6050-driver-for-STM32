46c6de26130f261413620757d508334100c91682
JuMalIO <44319139+JuMalIO@users.noreply.github.com>
2020-12-21 15:04:15 +0200

    Update mpu6050_defs.h

--- mpu6050_defs.h
+++ mpu6050_defs.h
@@ -389,8 +389,8 @@
  | [QUAT W][      ][QUAT X][      ][QUAT Y][      ][QUAT Z][      ] |
  |   0   1   2   3   4   5   6   7   8   9  10  11  12  13  14  15  |
  |                                                                  |
- | [GYRO X][GYRO Y][GYRO Z][ACC X ][ACC Y ][ACC Z ]									|
- |  16  17  18  19  20  21  22  23  24  25  26  27									|
+ | [GYRO X][GYRO Y][GYRO Z][ACC X ][ACC Y ][ACC Z ]                 |
+ |  16  17  18  19  20  21  22  23  24  25  26  27                  |
  * ================================================================ */
 
 // this block of memory gets written to the MPU on start-up, and it seems
